b15ac68c1a77029d9bdda31c5e8451a5872dc71f
edris-ahmed <edrisahmed70@gmail.com>
2023-10-05 17:10:05 +0200

    Add project files.

---
+++ Uppgift5/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift5
+{
+    public class Circle
+    {
+        public int radius {  get; set; }
+
+        public double GetArea()
+        {
+            return radius * radius * Math.PI;
+        }
+           
+    }
+}
---
+++ Uppgift5/Program.cs
@@ -0,0 +1,28 @@
+﻿//Edris Ahmed (NET23)
+
+namespace Uppgift5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle myCircle5 = new Circle()
+            {
+                radius = 5,
+            };
+
+            myCircle5.GetArea();
+            Console.WriteLine($"Arean för en cirkel med en radius av 5 är {myCircle5.GetArea()}.");
+
+            Circle myCircle6 = new Circle() 
+            { 
+                radius = 6, 
+            };
+
+            myCircle6.GetArea();
+            Console.WriteLine($"Arean för en cirkel med en radius av 6 är {myCircle6.GetArea()}.");
+
+
+        }
+    }
+}
